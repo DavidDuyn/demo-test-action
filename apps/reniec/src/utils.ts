c3929a7de4e7cefbcad314080a4688678ac23e30
DavidDuyn <davidduyn@facephi.com>
2022-09-05 12:31:19 +0200

    feat: test

--- apps/reniec/src/utils.ts
+++ apps/reniec/src/utils.ts
@@ -1,4 +1,5 @@
 export const getKeyByValue = (value: string, enumeration: any): string => {
   const indexOfValue = Object.values(enumeration).indexOf(value);
+  console.log(1);
   return Object.keys(enumeration)[indexOfValue];
 };
